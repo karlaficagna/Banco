a92f4ee9b6f9c9225a331916f737da2ec485e441
Karla Ficagna <karlaficagna@gmail.com>
2022-10-13 14:14:31 -0300

    final

--- app/src/main/java/br/com/dionataferraz/vendas/activity/ProfileActivity.kt
+++ app/src/main/java/br/com/dionataferraz/vendas/activity/ProfileActivity.kt
@@ -12,9 +12,12 @@ class ProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProfileBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         binding.btSave.setOnClickListener {
             Toast.makeText(this,  binding.rg.checkedRadioButtonId.toString(), Toast.LENGTH_LONG).show()
--- app/src/main/java/br/com/dionataferraz/vendas/login/data/local/UserEntity.kt
+++ app/src/main/java/br/com/dionataferraz/vendas/login/data/local/UserEntity.kt
@@ -2,15 +2,20 @@ package br.com.dionataferraz.vendas.login.data.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Room
+import br.com.dionataferraz.vendas.login.data.response.UserResponse
 
 @Entity(tableName = "userTable")
 data class UserEntity(
     val name: String,
 
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val email: String,
     val password: String,
-)
+) {
+
+}
 
 
 
